75d4eb1c918defa319d95792a6c9a8ae0a94ddcd
Yevgeniya Terlyuk <yterlyuk@fb.com>
2021-02-13 21:22:12 -0800

    created object for charachters and added functions to select all charachters

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -3,17 +3,70 @@
 WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria */
 
-
+passwordVariations = {
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  noLowerCase:"",
+  upperCase: "ABCDEFGHIGKLMNOPQRSTUVQXYZ",
+  noUpperCase:"",
+  numbers: "123456789",
+  noNumbers: "",
+  specialCharacters: "!@#$%^&*()_+=-",
+  noSpecialCharacters: ""
+}
 
 
 
 // I choose lowercase, uppercase, numeric, and/or special characters
-var promptCase = function(){
-  var lowerCase = window.confirm("Would you like your password to include lower case characters?");
-  if (lowerCase) {
-    promptCase = ["abcdefghijklmnopqrstuvwxyz"]
+var promptCase1 = function() {
+  var LowerCase = window.confirm("Would you like your password to include lower case characters?");
+  if (LowerCase) {
+    LowerCase = passwordVariations["lowerCase"];
+    console.log(LowerCase)
+    promptCase2()
   } else {
-    upperCase
+     LowerCase = passwordVariations["noLowerCase"];
+     console.log(LowerCase)
+     promptCase2()
+  }
+}
+
+var promptCase2 = function() {
+  var UpperCase = window.confirm("Would you like your password to include upper case characters?");
+  if (UpperCase) {
+    UpperCase = passwordVariations["upperCase"];
+    console.log(UpperCase)
+    promptCase3()
+    
+  } else {
+     UpperCase = passwordVariations["noUpperCase"];
+     console.log(UpperCase)
+     promptCase3()
+  }
+}
+
+var promptCase3 = function() {
+  var Numbers = window.confirm("Would you like your password to include numbers?");
+  if (Numbers) {
+    Numbers = passwordVariations["numbers"];
+    console.log(Numbers)
+    promptCase4()
+    
+  } else {
+     Numbers = passwordVariations["noNumbers"];
+     console.log(Numbers)
+     promptCase4()
+  }
+}
+
+var promptCase4 = function() {
+  var SpecialCharacters = window.confirm("Would you like your password to include special characters?");
+  if (SpecialCharacters) {
+    SpecialCharacters = passwordVariations["specialCharacters"];
+    console.log(SpecialCharacters)
+    
+  } else {
+     SpecialCharacters= passwordVariations["noSpecialCharacters"];
+     console.log(SpecialCharacters)
   }
 }
 
@@ -34,13 +87,13 @@ var promptCase = function(){
 var LenghtOfPassword = function() {
   var promptLenght = window.prompt("How many characters would you like your password to have?");
   if (promptLenght < 8) {
-    window.alert("Your password should have at lease 8 characters!")
+    window.alert("Your password should have at lease 8 characters")
     return LenghtOfPassword();
   } else if (promptLenght > 128) {
-    window.alert("Your password shout have less than 128 characters!")
+    window.alert("Your password should have less than 128 characters")
     return LenghtOfPassword();
   } else {
-    promptCase()
+    promptCase1()
   }
 }
 
